3a22bc34784e57dcd56aec8a46201a703c849f0a
kafrawy <mahmomohamed78@gmail.com>
2026-04-06 23:41:13 +0200

    lol finaly

---
+++ SFML_Template/ConsoleApplication1/Main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <SFML/Audio.hpp>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window = { VideoMode(1440,900),"sfml works" };
+    Event event;
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+    return 0;
+}
